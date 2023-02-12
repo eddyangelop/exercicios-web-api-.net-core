b6881e7ae9c1907f1b6c3f873532a1fc4cc538f7
eddyangelop <eddysoullive@gmail.com>
2023-02-12 08:51:29 -0300

    startup new template form 2

---
+++ WebApi_Net6/WebApi_Net6/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+
+namespace WebApi_Net6
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+        }
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+
+    }
+}
