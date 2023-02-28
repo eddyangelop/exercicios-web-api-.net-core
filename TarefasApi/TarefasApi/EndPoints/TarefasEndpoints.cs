19468335f6595a58ed1b2e1ab8169e7dfc5fb3f9
eddyangelop <eddysoullive@gmail.com>
2023-02-28 13:29:13 -0300

    definindo strings de conexão e implementando Endpoint Get

---
+++ TarefasApi/TarefasApi/EndPoints/TarefasEndpoints.cs
@@ -0,0 +1,27 @@
+﻿using Dapper.Contrib.Extensions;
+using System.Runtime.CompilerServices;
+using TarefasApi.Data;
+using static TarefasApi.Data.TarefaContext;
+
+namespace TarefasApi.EndPoints
+{
+    public static class TarefasEndpoints
+    {
+        public static void MapTarefasEndpoints(this WebApplication app)
+        {
+            app.MapGet("/", () => $"Bem-Vindo a API Tarefas {DateTime.Now}");
+
+            app.MapGet("/tarefas", async (GetConnection connectionGetter) =>
+            {
+                using var con = await connectionGetter();
+                var tarefas = con.GetAll<Tarefa>().ToList();
+
+                if (tarefas is null)
+                    return Results.NotFound();
+
+                return Results.Ok(tarefas);
+            });
+        }
+    }
+
+}
--- TarefasApi/TarefasApi/Program.cs
+++ TarefasApi/TarefasApi/Program.cs
@@ -1,11 +1,13 @@
+using TarefasApi.EndPoints;
 using TarefasApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddPersistence();
+
 var app = builder.Build();
 
-//app.MapTarefasEndpoints();
+app.MapTarefasEndpoints();
 
 
 app.Run();
