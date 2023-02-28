0fcbcddfe31dd13a611132283b3cdee044101bcf
eddyangelop <eddysoullive@gmail.com>
2023-02-28 14:09:23 -0300

    implementado crud da minimal Api

--- TarefasApi/TarefasApi/EndPoints/TarefasEndpoints.cs
+++ TarefasApi/TarefasApi/EndPoints/TarefasEndpoints.cs
@@ -21,7 +21,46 @@ namespace TarefasApi.EndPoints
 
                 return Results.Ok(tarefas);
             });
+
+            app.MapGet("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                //var tarefa = con.Get<Tarefa>(id);
+                //if (tarefa is null)
+                //return Results.NotFound();
+                //return Results.Ok(tarefa);
+
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+            });
+
+
+            app.MapPost("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(Tarefa);
+                return Results.Created($"/tarefas/{id}", Tarefa);
+            });
+
+            app.MapPut("/tarefas", async (GetConnection connectionGetter, Tarefa Tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(Tarefa);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id}", async (GetConnection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                var deleted = con.Get<Tarefa>(id);
+                if (deleted is null)
+                    return Results.NotFound();
+
+                con.Delete(deleted);
+                return Results.Ok(deleted);
+            });
+
         }
+
     }
 
 }
