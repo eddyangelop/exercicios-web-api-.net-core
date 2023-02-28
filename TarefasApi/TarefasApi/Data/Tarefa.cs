98e14eb3ddbf2c9b90591bcf5a243ea1a36ab50a
eddyangelop <eddysoullive@gmail.com>
2023-02-28 10:32:16 -0300

    criado tarefas e definido persistences

---
+++ TarefasApi/TarefasApi/Data/Tarefa.cs
@@ -0,0 +1,9 @@
+﻿// System.ComponentModel.DataAnnotations.Schema;
+using Dapper.Contrib.Extensions;
+
+namespace TarefasApi.Data;
+
+[Table("Tarefas")]
+public record Tarefa(int Id, string Atividade, string Status);
+
+
