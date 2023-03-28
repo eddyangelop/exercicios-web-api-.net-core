cd87fd5c412e54202ac815f5042d3e5181d6c1ed
eddyangelop <eddysoullive@gmail.com>
2023-03-28 18:19:36 -0300

    apicatalorg implementado endpoints produtos

--- ApiCatalogoOrg/ApiCatalogoOrg/Program.cs
+++ ApiCatalogoOrg/ApiCatalogoOrg/Program.cs
@@ -1,5 +1,6 @@
 using ApiCatalogoOrg.Context;
 using ApiCatalogoOrg.Models;
+using Microsoft.AspNetCore.Routing.Matching;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -78,6 +79,73 @@ app.MapDelete("/categorias/{id:int}", async (int id, AppDbContext db)
     }
 
     db.Categorias.Remove(Categoria);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+});
+
+//------------------------Endpoints para produtos------------------------------
+
+//Post
+app.MapPost("/produtos", async (Produto produto, AppDbContext db)
+    =>
+{
+    db.Produtos.Add(produto);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/produtos/{produto.ProdutoId}", produto);
+});
+
+//Get
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+
+//GetId
+app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db)
+    =>
+{
+    return await db.Produtos.FindAsync(id)
+                 is Produto produto
+                 ? Results.Ok(produto)
+                 : Results.NotFound();
+});
+
+//PutId
+app.MapPut("/produtos/{id:int}", async (int id, Produto produto, AppDbContext db)
+    =>
+{
+    if (produto.ProdutoId != id)
+    {
+        return Results.BadRequest();
+    }
+
+    var produtoDB = await db.Produtos.FindAsync(id);
+
+    if (produtoDB is null) return Results.NotFound();
+
+    produtoDB.Nome = produto.Nome;
+    produtoDB.Descricao = produto.Descricao;
+    produtoDB.Preco = produto.Preco;
+    produtoDB.Imagem = produto.Imagem;
+    produtoDB.DataCompra = produto.DataCompra;
+    produtoDB.Estoque = produto.Estoque;
+    produtoDB.CategoriaId = produto.CategoriaId;
+
+    await db.SaveChangesAsync();
+    return Results.Ok(produtoDB);
+});
+
+//DeleteId
+app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db)
+    =>
+{
+    var produto = await db.Produtos.FindAsync(id);
+
+    if (produto is null)
+    {
+        return Results.NotFound();
+    }
+
+    db.Produtos.Remove(produto);
     await db.SaveChangesAsync();
 
     return Results.NoContent();
