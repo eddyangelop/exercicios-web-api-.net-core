beb6dff67b3b058ddbb01897f921a089f7748d35
eddyangelop <eddysoullive@gmail.com>
2023-04-06 16:38:04 -0300

    Refatorando bloco de retorno da autorização Jwt na classe Services CollectionExtensions, persistence e autenticação; para organizar o ambiente de desenvolvimento do Swagger

--- ApiCatalogoOrg/ApiCatalogoOrg/Program.cs
+++ ApiCatalogoOrg/ApiCatalogoOrg/Program.cs
@@ -15,79 +15,16 @@ using System.Text;
 using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.//--> método ConfigureServices()
 
-// Add services to the container.
-builder.Services.AddEndpointsApiExplorer();
-
-
-// Bloco de autorização Jwt do Swagger
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "ApiCAtalogoOrg", Version = "v1" });
-
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = @"JWT Authorization header using the Bearer scheme.
-                   Enter 'Bearer'[space].Example: \'Bearer 12345abcdef\'",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-                    {
-                          new OpenApiSecurityScheme
-                          {
-                              Reference = new OpenApiReference
-                              {
-                                  Type = ReferenceType.SecurityScheme,
-                                  Id = "Bearer"
-                              }
-                          },
-                         new string[] {}
-                    }
-    });
-});
-
-
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-                 options
-                 .UseMySql(connectionString,
-                 ServerVersion.AutoDetect(connectionString)));
-
-builder.Services.AddSingleton<ITokenService>(new TokenService());
-
-builder.Services.AddAuthentication
-                 (JwtBearerDefaults.AuthenticationScheme)
-                 .AddJwtBearer(options =>
-                 {
-                     options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-                     {
-                         ValidateIssuer = true,
-                         ValidateAudience = true,
-                         ValidateLifetime = true,
-                         ValidateIssuerSigningKey = true,
-
-                         ValidIssuer = builder.Configuration["Jwt:Issuer"],
-                         ValidAudience = builder.Configuration["Jwt:Audience"],
-                         IssuerSigningKey = new SymmetricSecurityKey
-                         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-
-                     };
-                 });
-
-builder.Services.AddAuthorization();
-
+builder.AddApiSwagger();
+builder.AddPersistence();
+builder.Services.AddCors();
+builder.AddAutenticationJwt();
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.//--> m;etodo Configure()
 
-
+// Configure the HTTP request pipeline.//--> método Configure()
 app.MapAutenticacaoEndpoints();
 app.MapCategoriasEndpoints();
 app.MapProdutosEndpoints();
@@ -100,7 +37,6 @@ app.UseExceptionHandling(environment)
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 
 app.Run();
 
